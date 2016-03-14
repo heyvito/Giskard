0a1658fce026a9556003561fb19cc3523dcc79bd
Victor Gama <hey@vito.io>
2016-03-14 16:10:26 -0300

    Port old modules from R2D3

---
+++ bot_modules/sudo.js
@@ -0,0 +1,29 @@
+// $ Sudo
+// $ Authors: victorgama
+// $ Created on: Mon Mar 14 15:37:53 BRT 2016
+// - Test: This is a sample command description that will be parsed
+// and shown to users when they request help. Make sure to document
+// every command your module provides.
+
+var Base = require('../src/base_module');
+
+var Sudo = function(bot) {
+    Base.call(this, bot);
+    this.respond(/sudo echo #?([a-z0-9-_]+)\s(.+)$/, (response) => {
+            var requester = response.user;
+            console.log(requester);
+            if(requester && requester.isRoot()) {
+                this.searchChannel(response.match[1])
+                    .then((c) => {
+                        c.send(response.match[2]);
+                        response.reply('Done. :wink:');
+                    })
+                    .catch((ex) => {
+                        response.reply('Failed: ' + ex.message);
+                    });
+            }
+        });
+};
+
+Base.setup(Sudo);
+module.exports = Sudo;
