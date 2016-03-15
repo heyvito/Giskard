e5aa1e855b1227f63136c850901a63e3b6261a79
Victor Gama <hey@vito.io>
2016-03-15 10:25:58 -0300

    Move AdapterManager

--- src/managers/adapter_manager.js
+++ src/managers/adapter_manager.js
@@ -2,11 +2,20 @@ var fs = require('fs'),
     Path = require('path'),
     Promise = require('bluebird');
 
+/**
+ * Represents an object capable of listing and loading adapters using defined
+ * conventions.
+ */
 var Adapters = function() {
     this.basePath = Path.resolve(Path.join(__dirname, '..', 'adapters'));
 };
 
 Adapters.prototype = {
+
+    /**
+     * Gets a list of available adapters in the adapters folder.
+     * @return {String[]}           List containing the names of available adapters.
+     */
     getAdapters: function() {
         return fs.readdirSync(this.basePath)
             .map(d => Path.join(this.basePath, d))
@@ -14,6 +23,13 @@ Adapters.prototype = {
             .filter(d => fs.readdirSync(d).some(f => f === 'index.js'))
             .map(d => Path.basename(d));
     },
+
+    /**
+     * Loads an adapter.
+     * @param  {String} name Name of the adapter to be loaded.
+     * @return {Promise}     Promise that will be resolved when the given adapter is loaded.
+     *                       Can be rejected if there's a problem during the process.
+     */
     loadAdapter: function(name) {
         return new Promise((resolve, reject) => {
             var entrypoint = Path.join(this.basePath, name, 'index.js');
