a6b6d24760df351267dc6726ea4ce51abba16290
Victor Gama <hey@vito.io>
2016-03-17 18:49:44 -0300

    Remove unused model

--- src/utils/events.js
+++
@@ -1,9 +0,0 @@
-var i = 0,
-    events = {},
-    makeEvent = function(name) {
-        events[name] = `GISK_EVENT_${name.toString().toUpperCase()}_${i++}`;
-    };
-
-makeEvent('adapterReady');
-
-module.exports = events;
