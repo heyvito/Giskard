4643ad7e5d20f14720e5e6f93e15e846b2362280
Victor Gama <hey@vito.io>
2016-03-17 21:17:19 -0300

    Update sudo module

--- bot_modules/sudo.js
+++ bot_modules/sudo.js
@@ -7,19 +7,45 @@ var Base = require('../src/base_module');
 var Sudo = function(bot) {
     Base.call(this, bot);
     this.respond(/sudo echo #?([a-z0-9-_]+)\s(.+)$/, (response) => {
-            var requester = response.user;
-            console.log(requester);
-            if(requester && requester.isRoot()) {
-                this.searchChannel(response.match[1])
-                    .then((c) => {
-                        c.send(response.match[2]);
-                        response.reply('Done. :wink:');
-                    })
-                    .catch((ex) => {
-                        response.reply('Failed: ' + ex.message);
-                    });
-            }
+            response.getUser().then(u => {
+                if(u.isRoot()) {
+                    this.searchChannel(response.match[1])
+                        .then((c) => {
+                            c.send(response.match[2]);
+                            response.reply('Done. :wink:');
+                        })
+                        .catch((ex) => {
+                            response.reply('Failed: ' + ex.message);
+                        });
+                }
+            });
+        })
+        .respond(/sudo add-sudoer ([a-z0-9-_]+)/i, (response) => {
+            response.getUser().then(u => {
+                if(u.isRoot()) {
+                    this.searchUser(response.match[1])
+                        .then(u => {
+                            u.setRole('root')
+                                .then(() => response.reply('Done!'))
+                                .catch((ex) => response.reply(`Error: ${ex.message}`));
+                        })
+                        .catch((ex) => response.reply(`Error: ${ex.message}`));
+                }
+            });
+        })
+        .respond(/sudo rm-sudoer ([a-z0-9-_]+)/i, (response) => {
+            response.getUser().then(u => {
+                if(u.isRoot()) {
+                    this.searchUser(response.match[1])
+                        .then(u => {
+                            u.unsetRole('root');
+                            u.save().then(() => response.reply('Done!'))
+                                    .catch((ex) => response.reply(`Error: ${ex.message}`));
+                        })
+                        .catch((ex) => response.reply(`Error: ${ex.message}`));
+                }
         });
+    });
 };
 
 Base.setup(Sudo);
