d3bfdbb680bb54c53db2e5cc6057b2eb1aafc1b9
Victor Gama <hey@vito.io>
2016-03-20 21:45:06 -0300

    Implement Sentry utility module

---
+++ src/utils/sentry.js
@@ -0,0 +1,37 @@
+var Singleton = require('./singleton'),
+    raven = require('raven'),
+    logger = require('./logger')('Sentry');
+
+var Sentry = function() {
+    var key = (require('../models/settings').sharedInstance()).sentryUri;
+    if(key) {
+        this.client = new raven.Client(key);
+        logger.info('Ready.');
+    } else {
+        this.client = null;
+        logger.warning('Not working: Missing sentryUri setting key');
+    }
+}
+
+Sentry.prototype = {
+    setup: function() {
+        if(this.client) {
+            logger.verbose('Armed.');
+            this.client.patchGlobal();
+        }
+    },
+    recordError: function(ex) {
+        try {
+            if(this.client) {
+                this.client.captureException(ex);
+            }
+        } catch(ex) {
+            logger.error(ex);
+        }
+    },
+    recordEvent: function() {
+        return this.recordError.apply(this, arguments);
+    }
+}
+
+module.exports = new Singleton(Sentry);
