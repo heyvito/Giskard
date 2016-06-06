14689228a91934327620537a8930a005d020ef79
Victor Gama <hey@vito.io>
2016-06-06 14:07:48 -0300

    Add suspension capabilities to MessageComparator

--- src/models/message_comparator.js
+++ src/models/message_comparator.js
@@ -11,9 +11,11 @@ var logger = require('../utils/logger')('MessageComparator'),
  *                                  provided regex
  * @constructor
  */
-var MessageComparator = function(regex, callback) {
+var MessageComparator = function(moduleName, regex, callback) {
     this.regex = regex;
     this.callback = callback;
+    this.moduleName = moduleName;
+    this.suspended = false;
     logger.verbose('Registered message: ' + regex);
 };
 
@@ -25,6 +27,9 @@ MessageComparator.prototype = {
      * @return {Bool}                   Whether the callback was called or not
      */
     call: function(envelope) {
+        if(this.suspended) {
+            return false;
+        }
         var match = this.regex.exec(envelope.text);
         if(!!match) {
             logger.verbose('Message ' + envelope.text + ' matched regex ' + this.regex);
@@ -33,6 +38,16 @@ MessageComparator.prototype = {
             return true;
         }
         return false;
+    },
+
+    /**
+     * Sets the suspension state of this MessageComparator. Setting it to true, prevents it
+     * from matching messages and calling its module.
+     * @param {Boolean} newState New suspension state of this MessageComparator
+     * @since 2.0
+     */
+    setSuspensionState: function(newState) {
+        this.suspended = newState;
     }
 };
 
